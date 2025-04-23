bb92c03ce61e5642c31b11247227993b6327f1ab
Fatemeh Pourseifollah Noshahr <fatemehpourseifollahnoshahr@MacBook-Pro-di-Fatemeh.local>
2025-04-23 11:01:04 +0200

    First commit - snake game using turtle 🐍

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+import time
+
+from scoreboard import Scoreboard
+from snake import Snake
+from food import Food
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake game")
+screen.tracer(0)
+game_is_on = True
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    if snake.head.distance(food) < 15:
+        scoreboard.increase_score()
+        food.refresh()
+        snake.extend()
+
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) <10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
+
+
+
+screen.exitonclick()
